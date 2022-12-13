9ff6682a765293c3e49f28f787eda0fb72ab6a3c
scarstreet <71519078+scarstreet@users.noreply.github.com>
2022-12-13 21:32:46 +0800

    Made functional n accesible login UI

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <Logo class="pa-2" style="text-align: center"></Logo>
         <!-- <v-img
@@ -27,6 +23,22 @@
       </div>
 
       <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon color="white" v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in whichMenu"
+            :key="index"
+            @click="menuHandler(item.label)"
+          >
+            <v-list-item-title>{{ item.label }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
       <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -39,22 +51,69 @@
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <Login
+        :isDialogOpen="loginDialog"
+        :dialogMode="loginDialogMode"
+        v-on:change-dialog-mode="changeDialogMode"
+        v-on:close-dialog="loginDialog = false"
+      />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Logo from '@/components/Logo.vue'
+import Logo from "@/components/Logo.vue";
+import Login from "@/components/Login.vue";
 
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+  name: "App",
   components: {
-    Logo
-  }
+    Logo,
+    Login,
+  },
+  data: () => ({
+    loginDialog: false,
+    loginDialogMode: "",
+    menuDefault: [
+      { label: "Log In", cdt: true },
+      { label: "Register", cdt: true },
+    ],
+    menuLoggedIn: [
+      { label: "Profile", cdt: true },
+      { label: "Add Post", cdt: true },
+      { label: "Admin", cdt: true },
+      { label: "Log Out", cdt: true },
+    ],
+  }),
+  computed: {
+    isLoggedIn() {
+      return this.$store.state.isLoggedIn;
+    },
+    whichMenu() {
+      if (this.isLoggedIn) return this.menuLoggedIn;
+      return this.menuDefault;
+    },
+  },
+  methods: {
+    openLoginDialog() {
+      this.loginDialog = true;
+    },
+    changeDialogMode() {
+      if (this.loginDialogMode === 'Register')
+        this.loginDialogMode = 'Log In'
+      else
+        this.loginDialogMode = 'Register'
+    },
+    menuHandler(pressed) {
+      if (pressed == "Log In" || pressed == "Register") {
+        this.loginDialogMode = pressed;
+        this.loginDialog = true;
+      }
+    },
+    test() {
+      console.log("btn pressed");
+    },
+  },
 };
 </script>
--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,13 +1,134 @@
 <template>
-  <div></div>
+  <v-dialog
+    v-model="isDialogOpen"
+    scrollable
+    persistent
+    :overlay="false"
+    max-width="500px"
+    transition="dialog-transition"
+  >
+    <v-card width="500px" height="420px">
+      <v-toolbar absolute width="100%" color="black">
+        <v-toolbar-title style="color: white">{{ dialogMode }}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon color="white" @click="closeDialog()">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-container :style="'margin-top:'+marginTop+'px'">
+        <v-row align="center" justify="center" style="height:100%">
+          <v-col v-if="dialogMode == 'Register'" align="center" justify="center"  style="height:100%">
+            <v-form v-model="isFormValid">
+              <v-text-field
+                v-model="username"
+                :rules="[rules.required, rules.minLen]"
+                name="username"
+                label="Username"
+              ></v-text-field>
+              <v-text-field
+                :append-icon="pwShow ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="pwShow ? 'text' : 'password'"
+                :rules="[rules.required, rules.minLen]"
+                v-model="password"
+                name="password"
+                label="Password"
+                @click:append="pwShow = !pwShow"
+              ></v-text-field>
+              <v-text-field
+                :append-icon="repwShow ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="repwShow ? 'text' : 'password'"
+                :rules="[rules.required]"
+                v-model="rePassword"
+                name="repassword"
+                label="Re-confirm Password"
+                @click:append="repwShow = !repwShow"
+              ></v-text-field>
+            </v-form>
+            <v-btn :disabled="!isFormValid" color="black" style="color: white">{{ dialogMode }}</v-btn>
+            <p v-if="hasError" class="caption" style="color: red">error message goes here</p>
+            <v-btn text color="black" small @click="changeDialogMode()"
+              >Already have an account? Click here to log in</v-btn
+            >
+          </v-col>
+          <v-col v-else align="center" justify="center"  style="height:100%">
+            <v-form v-model="isFormValid">
+              <v-text-field
+                v-model="username"
+                :rules="[rules.required]"
+                name="username"
+                label="Username"
+              ></v-text-field>
+              <v-text-field
+                :append-icon="pwShow ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="pwShow ? 'text' : 'password'"
+                :rules="[rules.required]"
+                v-model="password"
+                name="password"
+                label="Password"
+                @click:append="pwShow = !pwShow"
+              ></v-text-field>
+            </v-form>
+            <v-btn :disabled="!isFormValid" color="black" style="color: white">{{ dialogMode }}</v-btn>
+            <p v-if="hasError" class="caption" style="color: red">error message goes here</p>
+            <v-btn text color="black" small @click="changeDialogMode()"
+              >Don't have an account? Click here to register!</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
-
-}
+  name: "Login",
+  props: {
+    isDialogOpen: Boolean,
+    dialogMode: String,
+  },
+  data: () => ({
+    pwShow: false,
+    repwShow: false,
+    hasError: false,
+    isFormValid: false,
+    username: "",
+    password: "",
+    rePassword: "",
+    rules: {
+      required: (v) => !!v || "This field may not be empty.",
+      minLen: (v) => v.length >= 4 || "Min 4 characters.",
+    },
+  }),
+  computed: {
+    isLoggedIn() {
+      return this.$store.state.isLoggedIn;
+    },
+    marginTop() {
+      if (this.dialogMode == 'Register')
+        return '70';
+      else return '120';
+    },
+    passwordSame() {
+      return this.password == this.rePassword;
+    },
+  },
+  methods: {
+    changeDialogMode() {
+      this.clearTextFields();
+      this.$emit('change-dialog-mode');
+    },
+    closeDialog() {
+      console.log(this.dialogMode);
+      this.$emit("close-dialog");
+    },
+    clearTextFields() {
+      this.username= "";
+      this.password= "";
+      this.rePassword= "";
+    }
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
